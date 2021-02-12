b43720886854699f6d3fbf6c6793e1877c426f89
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2021-02-12 00:16:20 -0700

    Add constructor for File

--- FileSystem/File.cs
+++ FileSystem/File.cs
@@ -9,6 +9,7 @@ namespace FileSystem
         public File() : base() { Size = "1 B"; }
         public File(string name) : base(name) { Size = "1 B"; }
         public File(string name, string size) : this(name) { Size = size; }
+        public File(string name, string size, string type) : this(name) { FileType = type; }
 
         public string FileType { get; set; }
     }
