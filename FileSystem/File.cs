88721e0ab1fe229d44ebe93a23a18e72c5e4f11d
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2021-02-11 23:33:19 -0700

    Shrink File constructors

--- FileSystem/File.cs
+++ FileSystem/File.cs
@@ -6,15 +6,9 @@ namespace FileSystem
 {
     class File : FileSystemObject
     {
-        public File() : base()
-        {
-            size = "1 B";
-        }
-
-        public File(string name) : base(name)
-        {
-            size = "1 B";
-        }
+        public File() : base() { size = "1 B"; }
+        public File(string name) : base(name) { size = "1 B"; }
+        public File(string name, string size) : this(name) { this.size = size; }
 
         public string type { get; set; }
     }
