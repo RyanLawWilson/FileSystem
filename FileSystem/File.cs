ce949d11d05eb82b6e68675071e9cee5f73d469f
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2021-02-14 01:33:18 -0700

    Change File constructor

--- FileSystem/File.cs
+++ FileSystem/File.cs
@@ -7,8 +7,7 @@ namespace FileSystem
     class File : FileSystemObject
     {
         public File() : base() { }
-        public File(string name) : base(name) { }
-        public File(string name, string type) : this(name) { FileType = type; }
+        public File(string name, string type = "") : base(name) { FileType = type; }
         public File(string name, string type, string size) : this(name, type) { Size = size; }
 
         public string FileType { get; set; }
