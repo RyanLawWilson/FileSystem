7058de9dc37e8e9dbfd81e2c125ae5071f2bbac0
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2021-02-14 02:04:31 -0700

    Add 'add' Command implementation and 'dir' Command

--- FileSystem/Command.cs
+++ FileSystem/Command.cs
@@ -28,12 +28,38 @@ namespace FileSystem.Comms
             switch (CommandWord)
             {
                 case "add":
-                    bool addCommandSwitch = false;
+                    //bool addCommandSwitch = false;
+                    if (sys.ActiveFolder.ContainedObjects == null)
+                    {
+                        sys.ActiveFolder.ContainedObjects = new List<FileSystemObject>();
+                    }
 
                     string fileSystemObjectName = words.Pop();
 
+                    string objectName;
+                    string fileType;
+                    // Detemine if the User is adding a file or folder.  Users that specify files should
+                    // type out the type of the file.  Users that want to add a folder should not specify a type.
+                    if (fileSystemObjectName.Contains('.'))
+                    {
+                        string[] splitInput = fileSystemObjectName.Split('.');
+                        if (splitInput.Length > 2)
+                        {
+                            //throw someException;
+                        }
+                        objectName = splitInput[0];
+                        fileType = splitInput[1];
 
+                        sys.ActiveFolder.ContainedObjects.Add(new File(objectName, fileType));
+                    }
+                    else
+                    {
+                        sys.ActiveFolder.ContainedObjects.Add(new Folder(fileSystemObjectName, sys.ActiveFolder));
+                    }
 
+                    break;
+                case "dir":
+                    Console.WriteLine("\n" + sys.ActiveFolder.Path());
                     break;
             }
         }
--- FileSystem/FileSystem.cs
+++ FileSystem/FileSystem.cs
@@ -52,7 +52,8 @@ namespace FileSystem
             Commands = new List<Command>()
             {
                 new Command("add", "Add", "Adds a file or folder to the file system based on the name provided"),
-                new Command("rem", "Remove", "Remove a file or folder that matches the name provided")
+                new Command("rem", "Remove", "Remove a file or folder that matches the name provided"),
+                new Command("dir", "Show Directory", "Shows all of the objects in the active folder")
             };
         }
 
